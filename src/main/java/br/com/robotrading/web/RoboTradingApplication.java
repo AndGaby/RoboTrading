c5b2f26ca8a7539a9ec52a00adee796774724e39
Anderson de Oliveira Santos <Anderson de Oliveira Santos>
2017-03-18 14:31:10 -0300

    Criando um projeto

---
+++ src/main/java/br/com/robotrading/web/RoboTradingApplication.java
@@ -0,0 +1,12 @@
+package br.com.robotrading.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboTradingApplication.class, args);
+	}
+}
