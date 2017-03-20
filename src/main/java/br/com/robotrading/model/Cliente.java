3557d9d0ebc1961b64b3d28b766f0fa9243ca5dc
AndGaby <gaby.rodrigues.castelo@gmail.com>
2017-03-19 21:51:59 -0300

    Back end do formulario de cadastro do cliente iniciado

---
+++ src/main/java/br/com/robotrading/model/Cliente.java
@@ -0,0 +1,168 @@
+package br.com.robotrading.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Entity
+@Table(name = "Clientes")
+public class Cliente {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private Long IdCli;
+
+	@NotBlank
+	@Length(min = 3, max = 45)
+	private String nomeCli;
+	
+//	@NotBlank
+//	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate DataNascCli;
+
+	@NotBlank
+	@Length(min = 3, max = 10)
+	private String userNameCli;
+
+//	@NotBlank
+//	@Length(min = 4, max = 30)
+	private String emailCli;
+
+//	@NotBlank
+	private String PasswordCli;
+
+//	@Length(min = 11, max = 11)
+	private Long CpfCli;
+
+//	@Length(min = 1, max = 40)
+	private String LogadouroCli;	
+
+//	@Length(min = 1, max = 10)
+	private Long LogadouroNumeroCli;
+
+//	@Length(min = 1, max = 10)
+	private Long CepCli;
+
+//	@Length(min = 1, max = 20)
+	private String CidadeCli;
+
+//	@Length(min = 2, max = 3)
+	private String EstadoCli;
+	
+//	@Length(min = 2, max = 20)
+	private String PaisCli;
+
+	public Long getIdCli() {
+		return IdCli;
+	}
+
+	public void setIdCli(Long idCli) {
+		IdCli = idCli;
+	}
+
+	public String getNomeCli() {
+		return nomeCli;
+	}
+
+	public void setNomeCli(String nomeCli) {
+		this.nomeCli = nomeCli;
+	}
+
+	public LocalDate getDataNascCli() {
+		return DataNascCli;
+	}
+
+	public void setDataNascCli(LocalDate dataNascCli) {
+		DataNascCli = dataNascCli;
+	}
+
+	public String getUserNameCli() {
+		return userNameCli;
+	}
+
+	public void setUserNameCli(String userNameCli) {
+		this.userNameCli = userNameCli;
+	}
+
+	public String getEmailCli() {
+		return emailCli;
+	}
+
+	public void setEmailCli(String emailCli) {
+		this.emailCli = emailCli;
+	}
+
+	public String getPasswordCli() {
+		return PasswordCli;
+	}
+
+	public void setPasswordCli(String passwordCli) {
+		PasswordCli = passwordCli;
+	}
+
+	public Long getCpfCli() {
+		return CpfCli;
+	}
+
+	public void setCpfCli(Long cpfCli) {
+		CpfCli = cpfCli;
+	}
+
+	public String getLogadouroCli() {
+		return LogadouroCli;
+	}
+
+	public void setLogadouroCli(String logadouroCli) {
+		LogadouroCli = logadouroCli;
+	}
+
+	public Long getLogadouroNumeroCli() {
+		return LogadouroNumeroCli;
+	}
+
+	public void setLogadouroNumeroCli(Long logadouroNumeroCli) {
+		LogadouroNumeroCli = logadouroNumeroCli;
+	}
+
+	public Long getCepCli() {
+		return CepCli;
+	}
+
+	public void setCepCli(Long cepCli) {
+		CepCli = cepCli;
+	}
+
+	public String getCidadeCli() {
+		return CidadeCli;
+	}
+
+	public void setCidadeCli(String cidadeCli) {
+		CidadeCli = cidadeCli;
+	}
+
+	public String getEstadoCli() {
+		return EstadoCli;
+	}
+
+	public void setEstadoCli(String estadoCli) {
+		EstadoCli = estadoCli;
+	}
+
+	public String getPaisCli() {
+		return PaisCli;
+	}
+
+	public void setPaisCli(String paisCli) {
+		PaisCli = paisCli;
+	}
+}
--- src/main/java/br/com/robotrading/web/RoboTradingApplication.java
+++ src/main/java/br/com/robotrading/web/RoboTradingApplication.java
@@ -2,9 +2,13 @@ package br.com.robotrading.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EntityScan(basePackages={"br.com.robotrading.model"})
+@EnableJpaRepositories(basePackages={"br.com.robotrading.dao"})
 public class RoboTradingApplication {
 
 	public static void main(String[] args) {
